b8435644e5ee33b54b0ccf92412aeb598669419f
Francisco d'Albertas <francisco.albertas@gmail.com>
2021-10-22 18:58:48 -0300

    mais do mesmo

--- panel_regression_certification.R
+++ panel_regression_certification.R
@@ -102,3 +102,76 @@ rm(pveg,pveg_df,pveg2)
 temp_data <- cbind(def_df_s,reg_df_s[,3],pveg_df_s[,3])
 
 names(temp_data)[4:5] <- c("reg_rate","p_veg")
+
+# exporting data
+
+write.csv(temp_data,"response_variables.csv",row.names = F)
+
+
+#==== tendencias temporais =====================================================
+
+# atribuindo dados 
+
+ma_2 <- left_join(ma,temp_data)
+ma_2$biome <- "Atlantic Forest"
+ce_2 <- left_join(ce,temp_data)
+ce_2$biome <- "Cerrado"
+df_full <- rbind(ma_2,ce_2)
+
+library(doBy)
+library(ggplot2)
+library(ggpubr)
+
+av <- summaryBy(data = df_full,def_rate_1+reg_rate+p_veg~treatment+year+biome) 
+
+
+def_year <- ggplot(data = av, 
+            aes(x = year, 
+                y = def_rate_1.mean,
+                group=treatment,
+                colour=treatment))+
+  #geom_point(aes(colour=treatment),alpha = 0.05)+
+  geom_line(size=0.8)+
+  #geom_ribbon(aes(x = year, ymin = lo.m01, ymax = up.m01), alpha = 1)+
+  #facet_wrap(~COD_IMOVEL, scales = "free") +
+  xlab("Year")+
+  ylab("deforestation rate")+
+  theme_bw()+
+  ylim(0,0.1)+
+  facet_grid("biome")
+  #scale_x_discrete(labels=c(seq(97,99,1),seq(0,18,1)))#+
+  #geom_vline(xintercept = 11,linetype = "dashed",colour="red",size=1)
+
+reg_year <- ggplot(data = av, 
+                   aes(x = year, 
+                       y = reg_rate.mean,
+                       group=treatment,
+                       colour=treatment))+
+  #geom_point(aes(colour=treatment),alpha = 0.05)+
+  geom_line(size=0.8)+
+  #geom_ribbon(aes(x = year, ymin = lo.m01, ymax = up.m01), alpha = 1)+
+  #facet_wrap(~COD_IMOVEL, scales = "free") +
+  xlab("Year")+
+  ylab("regeneration rate")+
+  theme_bw()+
+  ylim(0,0.1)+
+  facet_grid("biome")
+
+summary(df_full$p_veg)
+
+# padrão bizarro na razao de cobertura!
+
+prop_veg_year <- ggplot(data = av, 
+                   aes(x = year, 
+                       y = p_veg.mean,
+                       group=treatment,
+                       colour=treatment))+
+  #geom_point(aes(colour=treatment),alpha = 0.05)+
+  geom_line(size=0.8)+
+  #geom_ribbon(aes(x = year, ymin = lo.m01, ymax = up.m01), alpha = 1)+
+  #facet_wrap(~COD_IMOVEL, scales = "free") +
+  xlab("Year")+
+  ylab("vegetation cover ratio")+
+  theme_bw()+
+  #ylim(0,1)+
+  facet_grid("biome")
