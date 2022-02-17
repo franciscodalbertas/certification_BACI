dbb6bcb1e84eda4ae5e8507aeeb905291406aa64
Francisco d'Albertas <francisco.albertas@gmail.com>
2022-02-17 16:48:37 -0300

    substitui pveg pelo deficit no painel e pveg vai como suplementar!

--- figures_descriptive.R
+++ figures_descriptive.R
@@ -13,12 +13,11 @@ data_cert <- read.csv("data_for_panel_regression.csv")
 
 data_cert$first_year <- as.integer(substring(data_cert$Date.Issued, 1, 4))
 
-
 data_cert$first_year[is.na(data_cert$Date.Issued)] <- 0 # convert first year to NA
 
 dcert <- data_cert[data_cert$year > 2004, ]
 
-dcert$id <- as.numeric(as.factor(dcert$COD_IMOVEL))
+#dcert$id <- as.numeric(as.factor(dcert$COD_IMOVEL))
 
 dcert$certification_cat <- as.factor(dcert$certification_cat)
 
@@ -62,15 +61,39 @@ dce <- dce %>%
  
 df <- rbind(dma,dce)
 
+
+# adicionando area de app! pra descontar do deficit!
+
+# aqui tem q levar em consideracao mf! pra filtrar!mas pode ser depois
+
+library(sf)
+
+apps <- st_read(file.path("G:\\Meu Drive\\Doutorado\\cap3\\",
+                          "limites_propriedades","APPS.shp"))
+
+apps_f <- apps %>% filter(COD_IMOVEL %in% df$COD_IMOVEL)
+
+sf::sf_use_s2(FALSE)
+
+apps_f$APP_area_ha <- as.numeric(st_area(apps_f)/10^4)
+
+st_geometry(apps_f) <- NULL
+
+df_app <- left_join(df,apps_f)
+
+df_app$APP_area_ha[is.na(df_app$APP_area_ha)] <- 0
+
 ################################################################################
 
-#--------------------
+# continuar!
+
+#---------------------------------------------------------------
 
 # desmatamento
 
-#--------------------
+#---------------------------------------------------------------
 
-
+# faz com dados completos!
 
 df$treatment <- factor(df$treatment, levels = c("non certified","certified"))
 
@@ -118,11 +141,11 @@ desm_points <- ggpar(desm_points,
                  font.tickslab=(5),
                  x.text.angle = 90)
 
-#--------------------
+#-------------------------------------------------------------------------------
 
 # regeneracao
 
-#--------------------
+#------------------------------------------------------------------------------
 
 
 g2 <- df%>% filter(year>2003&year<=2017)%>%
@@ -165,14 +188,21 @@ reg_points <- ggpar(reg_points,
                      x.text.angle = 90)
 
 
-#--------------------
+#-------------------------------------------------------------------------------
 
 # veg cover
 
-#--------------------
+#-------------------------------------------------------------------------------
 
+# aqui faria sentido focar apenas nas medias e grandes!
 
-g3 <- df%>% filter(year>2003&year<=2017)%>%
+mf <- read.csv(file.path(dirname(getwd()),"certification_BACI","data_for_matching.csv"))
+
+df_app2 <- left_join(df_app,mf[,c(1,13)]) %>% filter(area_im_mf>=4)
+
+df_app2$treatment <- factor(df_app2$treatment, levels = c("non certified","certified"))
+
+g3 <- df_app2%>% filter(year>2003&year<=2017)%>%
   ggplot(aes(x = as.factor(year), y = p_veg, 
              color = treatment, group = interaction(as.factor(year), treatment))) +
   #coord_flip() +
@@ -214,14 +244,14 @@ pveg_points <- ggpar(pveg_points,
                     x.text.angle = 90)
 
 
-#--------------------
+#--------------------------------------------------------------------
 
 # papp
 
-#--------------------
+#---------------------------------------------------------------------
 
 
-g4 <- df%>% filter(year>2003&year<=2017)%>%
+g4 <- df_app2%>% filter(year>2003&year<=2017)%>%
   ggplot(aes(x = as.factor(year), y = papp, 
              color = treatment, group = interaction(as.factor(year), treatment))) +
   #coord_flip() +
@@ -262,14 +292,98 @@ app_points <- ggpar(app_points,
                      x.text.angle = 90)
 
 
+#--------------------------------------------------------------------
+
+# proportional deficit
+
+#---------------------------------------------------------------------
+
+# calculando deficit fora de APP
+
+df_app2 <- df_app2 %>% 
+  mutate(deficit_noAPP= (area_im* 0.2)- (area_im* p_veg)- (APP_area_ha)) %>%
+  mutate(deficit_noAPP = replace(deficit_noAPP, deficit_noAPP<0, 0)) %>%
+  mutate(deficit_noAPP_prop= deficit_noAPP/area_im)
+
+
+g5 <- df_app2%>% filter(year>2003&year<=2017)%>%
+  ggplot(aes(x = as.factor(year), y = deficit_noAPP_prop, 
+             color = treatment, group = interaction(as.factor(year), treatment))) +
+  #coord_flip() +
+  coord_cartesian(ylim = c(0,0.2)) +
+  #scale_color_uchicago() +
+  labs(x = NULL, y = "vegetation deficit") +
+  theme(
+    legend.position = "none",
+    axis.title = element_text(size = 16),
+    axis.text.x = element_text(family = "Roboto Mono", size = 12),
+    panel.grid = element_blank())+
+  scale_x_discrete()+
+  annotate(geom="rect",
+           xmin=5, 
+           xmax=9, 
+           ymin=0, 
+           ymax=0.25,
+           fill="yellow",
+           alpha=0.2,
+           color=NA)
+
+
+deficit_points <- g5 +
+  geom_jitter(colour = "gray",size = 1, alpha = 0.1, width = 0.2) +
+  stat_summary(fun = mean, geom = "point", size = 2,stroke = 0.5,shape=21)+
+  theme_classic()+facet_wrap(~biome)+
+  theme(strip.background = element_blank())
+#scale_x_discrete(labels=label_x)
+
+deficit_points <- ggpar(deficit_points,
+                    legend = "none",
+                    #xlab = "time (years)",
+                    #ylab = "average effect on vegetation cover",
+                    main = "",
+                    font.x=c(7,"bold"),
+                    font.y=c(7,"bold"),
+                    font.tickslab=(5),
+                    x.text.angle = 90)
+
+
+
+
+
+
+
+
+
+
+
+#-------------------------------------------------------------------------
+
+# painel antigo
 panel <- ggarrange(desm_points,reg_points,pveg_points,app_points,common.legend = T,
                    labels = "auto",ncol = 2,nrow = 2)
 
-# versao com faixa de adocao certificacao
+# painel com deficit
+
+painel_deficit <- ggarrange(desm_points,reg_points,deficit_points,app_points,common.legend = T,
+                            labels = "auto",ncol = 2,nrow = 2)
+
+
+# essa era antes de ter a faixa!
 
 ggsave(filename = file.path("figures","descriptive_figure.jpeg"),plot = panel,width = 14,
        height = 12,units = "cm")
 
+# versao com faixa de adocao certificacao
+
 ggsave(filename = file.path("figures","descriptive_figure_comfaixa.jpeg"),plot = panel,width = 14,
        height = 12,units = "cm")
 
+
+ggsave(filename = file.path("figures","descriptive_figure_comfaixa_deficit.jpeg"),plot = painel_deficit,width = 14,
+       height = 12,units = "cm")
+
+# salvando figura de cobertura de vegetacao como suplementar!
+
+
+ggsave(filename =file.path("figures","descriptive_figure_comfaixa_pveg.jpeg"),plot =pveg_points,
+       width = 6,height = 6, units = "cm")
